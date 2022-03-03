452943c72d824468f8d9b6484314d34ff2184d99
elyghthao <33295950+elyghthao@users.noreply.github.com>
2022-03-03 10:24:49 -0800

    update

--- src/my_game/my_game.js
+++ src/my_game/my_game.js
@@ -1,66 +1,65 @@
 "use strict";  // Operate in Strict mode such that variables must be declared before used!
 
 import engine from "../engine/index.js";
-
-import Minion from "./game_objects/minion.js";
-
+import Minion from "./objects/minion.js";
 class MyGame extends engine.Scene {
     constructor() {
         super();
 
         // The camera to view the scene
         this.mCamera = null;
-        // this.kMinionSprite = "assets/SpriteSheet.png";
-        this.kMinionSprite = "./assets/SpriteSheet.png";
-        
-        
-        this.mMinion = null;
-        this.init();
-    }
 
+        this.mMsg = null;
+    
+        this.kMinionSprite = "assets/minion_sprite.png";
+        this.mMinion = null;
+    }
     load() {
-        
         engine.texture.load(this.kMinionSprite);
     }
-
     unload() {
         engine.texture.unload(this.kMinionSprite);
     }
-        
+
     init() {
-        
+        // Step A: set up the cameras
         this.mCamera = new engine.Camera(
-            vec2.fromValues(30, 27.5), // position of the camera
+            vec2.fromValues(30, 30), // position of the camera
             100,                       // width of camera
             [0, 0, 640, 480]           // viewport (orgX, orgY, width, height)
         );
         this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
-        
-        this.mMinion = new Minion(this.kMinionSprite);
+                // sets the background to gray
+    
+        this.mMsg = new engine.FontRenderable("Status Message");
+        this.mMsg.setColor([0, 0, 0, 1]);
+        this.mMsg.getXform().setPosition(-19, -8);
+        this.mMsg.setTextHeight(3);
+
+        this.mMinion = new Minion(this.kMinionSprite,-10,20,.2);
     }
     
-    // This is the draw function, make sure to setup proper drawing environment, and more
-    // importantly, make sure to _NOT_ change any state.
+    
+    _drawCamera(camera) {
+        camera.setViewAndCameraMatrix();
+        this.mMinion.draw(camera);
+    }
     draw() {
         // Step A: clear the canvas
         engine.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
+    
         this.mCamera.setViewAndCameraMatrix();
-        this.mMinion.draw(this.mCamera);
+        
+        this.mMsg.draw(this.mCamera);   // only draw status in the main camera
+        this._drawCamera(this.mCamera);
+        
     }
     
     // The Update function, updates the application state. Make sure to _NOT_ draw
+    // anything from this function!
     update () {
-        //Object is moving right
-        
-        
-        //enter freeze state
-        //enter input
-        //exit freeze state
-        //object does what freeze state described
-
-        console.log("HI");
-
-        
+       this.mMinion.update();
+        this.mMsg.setText("HI");
     }
 }
 
---
+++ src/my_game/objects/minion.js
@@ -0,0 +1,46 @@
+"use strict";
+
+import engine from "../../engine/index.js";
+
+class Minion extends engine.GameObject {
+    constructor(spriteTexture, atX, atY, cooldown) {
+        super(null);
+        this.kDelta = 0.3;
+
+        this.mRenderComponent = new engine.SpriteAnimateRenderable(spriteTexture);
+        this.mRenderComponent.setColor([1, 1, 1, 0]);
+        this.mRenderComponent.getXform().setPosition(atX, atY);
+        this.mRenderComponent.getXform().setSize(-8, 6);
+        this.mRenderComponent.setSpriteSequence(512, 0,      // first element pixel position: top-left 512 is top of image, 0 is left of image
+            204, 164,   // width x height in pixels
+            5,          // number of elements in this sequence
+            0);   
+
+        this.cooldown = cooldown;
+        this.start = performance.now();
+        this.animateLegs = true;
+    }
+
+    update() {
+        let xForm = this.mRenderComponent.getXform();
+        xForm.incXPosBy(.05);
+        this.animate();
+        // xForm.incXPosBy(.005);
+        
+    }
+
+    animate() {
+        let cd = performance.now();
+
+        if(this.animateLegs) {
+            if(cd-this.start > this.cooldown*1000) {
+                this.mRenderComponent.updateAnimation();
+                this.start = performance.now();
+            }
+        }
+        
+        
+    }
+}
+
+export default Minion;
